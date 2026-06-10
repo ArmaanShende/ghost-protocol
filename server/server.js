d64f6d1c383dd78bd2c0986597436746c2c5e3e4
ArmaanShende <armaanshende05@gmail.com>
2026-06-10 22:48:06 +0530

    feat: connect MongoDB Atlas via Mongoose (#1)

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 // Load environment variables
 dotenv.config()
+
+// Connect to database
+connectDB()
 
 // Create Express app
 const app = express()
@@ -18,7 +22,7 @@ app.get('/', (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
